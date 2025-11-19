35b87464ad56ea82a5df4acde9ba99004f23e44b
xaxier41-tech <xaxier41@gmail.com>
2025-11-19 17:51:05 -0600

    Delete api directory

--- api/notify.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    // Send phone notification here (Discord webhook, Telegram, etc.)
-    console.log("Button pressed!");
-    return res.status(200).json({ message: "Notification received!" });
-  }
-
-  res.status(405).json({ error: "Method not allowed" });
-}
